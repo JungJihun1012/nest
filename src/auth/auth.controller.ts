9f9acae9740837ca6fb57ff4e652aac319e15911
JungJihun1012 <jihunjeong965@gmail.com>
2025-07-24 22:09:05 +0900

    feat: jwt 토큰 생성

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,21 +2,34 @@ import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import { AuthDTO } from './dto/auth.dto';
 import * as bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('user')
 export class AuthController {
-  constructor(private readonly userService: UsersService) {}
+  constructor(
+    private readonly userService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   @Post('/signin')
   async signin(@Body() authDTO: AuthDTO.SignIn) {
     const { email, password } = authDTO;
 
     const user = await this.userService.findByEmail(email);
-    const isSamePassword = bcrypt.compareSync(password, email);
-    if (!user && !isSamePassword) {
+    if (!user) {
+      throw new UnauthorizedException('이메일 또는 비밀번호를 확인해주세요.');
+    }
+    const isSamePassword = bcrypt.compareSync(password, user.password);
+    if (!isSamePassword) {
       throw new UnauthorizedException('이메일 또는 비밀번호를 확인해주세요.');
     }
 
-    return '로그인 완료';
+    const payload = {
+      id: user.id,
+    };
+
+    const accessToken = this.jwtService.sign(payload);
+
+    return accessToken;
   }
 }
--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: 'secret',
+      signOptions: { expiresIn: '300s' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
